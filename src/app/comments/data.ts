01de7fb76e72e23e923a0c44683a2c05de774acf
teyim <teyimasobo@gmail.com>
2024-01-31 19:50:54 +0100

    route handler POST request

--- src/app/comments/data.ts
+++ src/app/comments/data.ts
@@ -1,14 +1,14 @@
 export const comments = [
   {
     id: 1,
-    test: "This is the first comment",
+    text: "This is the first comment",
   },
   {
     id: 2,
-    test: "This is the second comment",
+    text: "This is the second comment",
   },
   {
     id: 3,
-    test: "This is the third comment",
+    text: "This is the third comment",
   },
 ];
--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,19 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const commentData = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: commentData.text,
+  };
+  comments.push(newComment);
+
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-type": "application/json",
+    },
+    status: 201,
+  });
+}
