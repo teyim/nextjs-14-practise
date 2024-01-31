0c79dc2de92128f008837500d1b71368cb8ef6a1
teyim <teyimasobo@gmail.com>
2024-01-31 19:42:36 +0100

    handling get request in route

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    test: "This is the first comment",
+  },
+  {
+    id: 2,
+    test: "This is the second comment",
+  },
+  {
+    id: 3,
+    test: "This is the third comment",
+  },
+];
